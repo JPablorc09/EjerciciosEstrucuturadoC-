1cf50a9c9851f26dfc88f8a990d19482d007a37e
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:31:57 -0600

    Add files via upload

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una letra: ");
+            char vocal =char.Parse(Console.ReadLine());
+            Console.ReadKey();
+            if (esVocal(vocal))
+            {
+                Console.WriteLine("La letra ingresada es una vocal");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("la letra no es una vocal");
+                Console.ReadKey();
+            }
+        }
+        static bool esVocal(char vocal)
+        {
+            vocal=char.ToLower(vocal);
+
+            switch (vocal)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
