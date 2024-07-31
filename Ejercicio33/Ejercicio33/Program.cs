7301f4e354b6936c2ba3c7f97317dc30332f6f66
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:41:30 -0600

    Add files via upload

---
+++ Ejercicio33/Ejercicio33/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir la contraseña correcta
+            string contraseñaCorrecta = "Contra123";
+            int intentosMaximos = 3;
+            int intentos = 0;
+            bool contrasenaCorrecta = false;
+
+            Console.WriteLine("Ingrese la contraseña:");
+
+            while (intentos < intentosMaximos && !contrasenaCorrecta)
+            {
+                // Leer la contraseña ingresada por el usuario
+                string contraseñaIngresada = Console.ReadLine();
+
+                // Comparar la contraseña ingresada con la correcta
+                if (contraseñaIngresada == contraseñaCorrecta)
+                {
+                    contrasenaCorrecta = true;
+                    Console.WriteLine("Contraseña correcta. Acceso concedido.");
+                }
+                else
+                {
+                    intentos++;
+                    if (intentos < intentosMaximos)
+                    {
+                        Console.WriteLine($"Contraseña incorrecta. Te quedan {intentosMaximos - intentos} intentos.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Has excedido el número máximo de intentos. Acceso denegado.");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
