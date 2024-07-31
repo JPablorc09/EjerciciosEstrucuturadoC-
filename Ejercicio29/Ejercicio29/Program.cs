c7a7b45ff29d6bae544a467e30d4f149ae9ef13e
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:40:40 -0600

    Add files via upload

---
+++ Ejercicio29/Ejercicio29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i += 3)
+            {
+                // Verificar si el número es múltiplo de 3
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
