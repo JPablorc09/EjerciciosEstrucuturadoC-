c8473f3575393e33e96a2047009f953810d3e382
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:36:17 -0600

    Add files via upload

---
+++ Ejercicio23/Ejercicio23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero del 1 en adelante");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor ingrese un valor mayo a cero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
