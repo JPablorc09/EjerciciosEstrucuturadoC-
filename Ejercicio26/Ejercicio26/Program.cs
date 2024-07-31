30bccd39eb51a07bc5e1bd2091ab0a1a42eec021
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:37:35 -0600

    Add files via upload

---
+++ Ejercicio26/Ejercicio26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar los números pares entre 0 y 100
+            Console.WriteLine("Números pares entre 0 y 100:");
+
+            // Iterar desde 0 hasta 100
+            for (int i = 0; i <= 100; i++)
+            {
+                // Verificar si el número es par
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
