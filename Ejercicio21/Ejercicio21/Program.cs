a9cdc2c0d3a1f94ef78aa3fda4dddc13ac32f301
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:35:42 -0600

    Add files via upload

---
+++ Ejercicio21/Ejercicio21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar viable
+            double num;
+            // Solicitar al usuario que ingrese un número
+            Console.WriteLine("Ingrese un numero");
+            num=double.Parse(Console.ReadLine());
+
+            // Calcular el cuadrado y el cubo del número
+            double cuadrado = num * num;
+            double cubo = num * num * num;
+
+            // Mostrar los resultados
+            Console.WriteLine("El cuadrado de {0} es {1}",num,cuadrado);
+            Console.WriteLine("El cubo de {0} es {1}",num,cubo);
+            Console.ReadKey();
+
+        }
+    }
+}
