0e7307445375355b5010e10742c97c95fdcbceeb
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:40:56 -0600

    Add files via upload

---
+++ Ejercicio30/Ejercicio30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mostrar los múltiplos de 3 y de 2 entre 0 y 100
+            Console.WriteLine("Números entre 0 y 100 que son múltiplos de 3 y 2:");
+
+            // Iterar desde 0 hasta 100
+            for (int i = 0; i <= 100; i++)
+            {
+                // Verificar si el número es múltiplo tanto de 3 como de 2
+                if (i % 3 == 0 && i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
