b3d1fb79c4ff5ecf11c84cdbe6b18ae411844e00
Juan <106122910+JPablorc09@users.noreply.github.com>
2024-07-31 17:33:05 -0600

    Add files via upload

---
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una frase no más de 20 caracteres:");
+            string frase = Console.ReadLine();
+
+            // Validar que la frase tenga como máximo 20 caracteres
+            if (frase.Length > 20)
+            {
+                Console.WriteLine("La frase ingresada tiene más de 20 caracteres. Inténtelo de nuevo.");
+                return;
+            }
+
+            int conteoVocales = ContarVocales(frase);
+
+            Console.WriteLine($"La frase \"{frase}\" tiene {conteoVocales} vocales.");
+            Console.ReadLine();
+        }
+        public static int ContarVocales(string frase)
+        {
+            int contador = 0;
+            string vocales = "aeiouAEIOU";
+
+            foreach (char c in frase)
+            {
+                if (vocales.Contains(c))
+                {
+                    contador++;
+                }
+            }
+
+            return contador;
+        }
+        
+    }
+}
